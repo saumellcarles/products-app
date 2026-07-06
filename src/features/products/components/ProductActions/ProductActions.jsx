0bde856f0c09272172885f92c5af9d19e4ecb1fb
Carles Saumell <saumellcarles@gmail.com>
2026-07-06 16:57:52 +0200

    feat: implement color selector

--- src/features/products/components/ProductActions/ProductActions.jsx
+++ src/features/products/components/ProductActions/ProductActions.jsx
@@ -3,12 +3,15 @@ import { Select } from '../../../../shared/components';
 import styles from './ProductActions.module.scss';
 
 const STORAGE_LABEL = 'Almacenamiento';
+const COLOR_LABEL = 'Color';
 
 export function ProductActions({ product }) {
-  const { storages } = product.options;
+  const { storages, colors } = product.options;
   const [selectedStorageCode, setSelectedStorageCode] = useState(storages[0].code);
+  const [selectedColorCode, setSelectedColorCode] = useState(colors[0].code);
 
   const storageOptions = storages.map(({ code, name }) => ({ value: code, label: name }));
+  const colorOptions = colors.map(({ code, name }) => ({ value: code, label: name }));
 
   return (
     <div className={styles.actions}>
@@ -17,6 +20,12 @@ export function ProductActions({ product }) {
         options={storageOptions}
         value={selectedStorageCode}
         onChange={(value) => setSelectedStorageCode(Number(value))}
+      />
+      <Select
+        label={COLOR_LABEL}
+        options={colorOptions}
+        value={selectedColorCode}
+        onChange={(value) => setSelectedColorCode(Number(value))}
       />
     </div>
   );
